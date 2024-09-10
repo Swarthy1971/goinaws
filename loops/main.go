fffcf0f40b9b34177fce516ba773d40b9e67a74c
Swarthy1971 <marek.turadek@onet.pl>
2024-09-10 16:57:27 +0200

    (feat) structs, pointers and references

--- loops/main.go
+++ loops/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 func main() {
@@ -21,7 +20,23 @@ func main() {
 		"cat",
 	}
 	animals = append(animals, "bird")
-	animals = slices.Delete(animals, 0, 1)
+	//animals = slices.Delete(animals, 0, 1)
 	fmt.Println(animals)
 
+	for i := 0; i < len(animals); i++ {
+		fmt.Println(animals[i])
+	}
+
+	for index, value := range animals {
+		fmt.Printf("index: %d value: %s\n", index, value)
+	}
+	for value := range 10 {
+		fmt.Println(value)
+	}
+
+	i := 0
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}
 }
